7b7c4320a21900720efd137040342a41dd421b6d
Vegetab1eDog <xuyang21@mails.jlu.edu.cn>
2025-04-11 10:00:49 +0800

    commit

--- src/main/java/com/zxy/advanced/TestIterator.java
+++ src/main/java/com/zxy/advanced/TestIterator.java
@@ -6,16 +6,29 @@ import java.util.Iterator;
 public class TestIterator {
     public static void main(String[] args) {
         // 创建集合
-        ArrayList<String> sites = new ArrayList<String>();
-        sites.add("aaa");
-        sites.add("bbb");
-        sites.add("ccc");
-        sites.add("ddd");
+        ArrayList<Integer> sites = new ArrayList<Integer>();
+        sites.add(111);
+        sites.add(222);
+        sites.add(333);
+        sites.add(444);
+        sites.add(555);
+        sites.add(666);
 
         // 获取迭代器
-        Iterator<String> it = sites.iterator();
+        Iterator<Integer> it = sites.iterator();
 
         // 输出集合中的第一个元素
         System.out.println(it.next());
+        System.out.println("\n");
+
+        while(it.hasNext()) {
+            Integer next = it.next();
+            System.out.println(next);
+            if(next<333){
+                it.remove();
+            }
+        }
+        System.out.println("\n");
+        System.out.println(sites);
     }
 }
